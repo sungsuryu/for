c48eb2b69de3c453a41b4ab8103c502c0e3d755c
sungsuryu <sungsu.ryu@gmail.com>
2021-04-26 14:49:58 +0900

    사용자 테이블 ddl추가

---
+++ forexcg/src/main/java/egovframework/knia/foreign/exchange/vo/UserVO.java
@@ -0,0 +1,14 @@
+package egovframework.knia.foreign.exchange.vo;
+
+import java.io.Serializable;
+
+public class UserVO implements Serializable {
+
+	private static final long serialVersionUID = -432319567215992186L;
+
+	private String userId;
+	
+	private String password;
+	
+	
+}
