e75e3fe2e31c3ba041194dde137fb2d36d2745ad
sungsuryu <sungsu.ryu@gmail.com>
2021-04-26 17:34:28 +0900

    login 화면 샘플링

---
+++ forexcg/src/main/java/egovframework/knia/foreign/exchange/controller/auth/LoginController.java
@@ -0,0 +1,28 @@
+package egovframework.knia.foreign.exchange.controller.auth;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import egovframework.knia.foreign.exchange.vo.LoginVO;
+
+@Controller
+public class LoginController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
+	
+	@RequestMapping(value="/login.do")
+	public String login(@ModelAttribute("loginVO") LoginVO loginVO, HttpServletRequest request, ModelMap model) throws Exception {
+		
+		LOGGER.info("-------------------------");
+		
+		return "usr/login";
+	}
+}
