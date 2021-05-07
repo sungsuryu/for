736cc321f62f83f033edc7791d05c36a5c91af12
sungsuryu <sungsu.ryu@gmail.com>
2021-05-07 13:51:59 +0900

    Login Interceptor 추가, LoginVO String ip 추가함.

---
+++ forexcg/src/main/java/egovframework/com/cmm/interceptor/IpObtainInterceptor.java
@@ -0,0 +1,44 @@
+package egovframework.com.cmm.interceptor;
+ 
+import egovframework.knia.foreign.exchange.vo.LoginVO;
+
+import egovframework.com.cmm.util.EgovUserDetailsHelper;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+ 
+/**
+ * 사용자IP 체크 인터셉터
+ * @author 유지보수팀 이기하
+ * @since 2013.03.28
+ * @version 1.0
+ * @see
+ *  
+ * <pre>
+ * << 개정이력(Modification Information) >>
+ * 
+ *   수정일     수정자          수정내용
+ *  ----------  --------    ---------------------------
+ *  2013.03.28	이기하          최초 생성
+ *  2021.05.07	류성수          loginVO 교체 
+ *  </pre>
+ */
+
+public class IpObtainInterceptor extends HandlerInterceptorAdapter {
+ 
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+ 
+		String clientIp = request.getRemoteAddr();
+ 
+		LoginVO loginVO = (LoginVO) EgovUserDetailsHelper.getAuthenticatedUser();
+ 
+		if (loginVO != null) {
+			loginVO.setIp(clientIp);
+		}
+ 
+		return true;
+	}
+}
