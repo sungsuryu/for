897059ecef478e5cf2cf70ec044f3b6c162b18bc
sungsuryu <sungsu.ryu@gmail.com>
2021-05-10 13:41:53 +0900

    example 파일 제거

---
+++ forexcg/src/main/java/egovframework/knia/foreign/exchange/cmm/code/CommonConst.java
@@ -0,0 +1,12 @@
+package egovframework.knia.foreign.exchange.cmm.code;
+
+public final class CommonConst {
+
+	public static final int LOGIN_STEP0 = 0;
+	
+	public static final int LOGIN_STEP1 = 1;
+	
+	public static final int LOGIN_STEP2 = 2;
+	
+	public static final int LOGIN_STEP9 = 9;
+}
