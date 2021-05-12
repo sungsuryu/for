c38a4b77b3619490a6ab32e6cd67dfbe046be2bc
sungsuryu <sungsu.ryu@gmail.com>
2021-05-12 13:05:34 +0900

    화면 퍼블리싱 업데이트

--- forexcg/src/main/webapp/js/design.js
+++ forexcg/src/main/webapp/js/design.js
@@ -17,6 +17,11 @@ $(function(){
 		$('#aside_right').removeClass('on');
 	});
 	$('#aside_left .gnb .depth1 > a').click(function(){
+		$('#aside_left .gnb .depth1').removeClass('on');
 		$(this).parent('.depth1').toggleClass('on');
+	});
+	$('#aside_left .gnb .depth2 > a').click(function(){
+		$('#aside_left .gnb .depth2').removeClass('on');
+		$(this).parent('.depth2').toggleClass('on');
 	});
 });
