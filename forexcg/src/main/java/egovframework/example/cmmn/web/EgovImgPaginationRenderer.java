0bdc83d2ee98449ef31ceec6c0f86560eb926e6e
jwKang <jwKang@DESKTOP-Q7STJ8P>
2021-05-25 09:44:14 +0900

    페이지 네이터 수정작업 완료 -KJW-

--- forexcg/src/main/java/egovframework/example/cmmn/web/EgovImgPaginationRenderer.java
+++ forexcg/src/main/java/egovframework/example/cmmn/web/EgovImgPaginationRenderer.java
@@ -16,6 +16,9 @@
 package egovframework.example.cmmn.web;
 
 import egovframework.rte.ptl.mvc.tags.ui.pagination.AbstractPaginationRenderer;
+import egovframework.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
+
+import java.text.MessageFormat;
 
 import javax.servlet.ServletContext;
 
@@ -75,5 +78,55 @@ public class EgovImgPaginationRenderer extends AbstractPaginationRenderer implem
 	public void setServletContext(ServletContext servletContext) {
 		this.servletContext = servletContext;
 		initVariables();
+	}
+
+	@Override
+	public String renderPagination(PaginationInfo paginationInfo, String jsFunction) {
+		StringBuffer strBuff = new StringBuffer();
+
+		int firstPageNo = paginationInfo.getFirstPageNo();
+		int firstPageNoOnPageList = paginationInfo.getFirstPageNoOnPageList();
+		int totalPageCount = paginationInfo.getTotalPageCount();
+		int pageSize = paginationInfo.getPageSize();
+		int lastPageNoOnPageList = paginationInfo.getLastPageNoOnPageList();
+		int currentPageNo = paginationInfo.getCurrentPageNo();
+		int lastPageNo = paginationInfo.getLastPageNo();
+
+		if (totalPageCount > pageSize) {
+			if (firstPageNoOnPageList > pageSize) {
+				strBuff.append(MessageFormat.format(firstPageLabel, new Object[] { jsFunction, Integer.toString(firstPageNo) }));
+				strBuff.append(MessageFormat.format(previousPageLabel, new Object[] { jsFunction, Integer.toString(firstPageNoOnPageList - 1) }));
+			} else {
+				strBuff.append(MessageFormat.format(firstPageLabel, new Object[] { jsFunction, Integer.toString(firstPageNo) }));
+				strBuff.append(MessageFormat.format(previousPageLabel, new Object[] { jsFunction, Integer.toString(firstPageNo) }));
+			}
+		}
+		else{
+			strBuff.append(MessageFormat.format(firstPageLabel, new Object[] { jsFunction, Integer.toString(firstPageNo) }));
+			strBuff.append(MessageFormat.format(previousPageLabel, new Object[] { jsFunction, Integer.toString(firstPageNo) }));
+		}
+
+		for (int i = firstPageNoOnPageList; i <= lastPageNoOnPageList; i++) {
+			if (i == currentPageNo) {
+				strBuff.append(MessageFormat.format(currentPageLabel, new Object[] { Integer.toString(i) }));
+			} else {
+				strBuff.append(MessageFormat.format(otherPageLabel, new Object[] { jsFunction, Integer.toString(i), Integer.toString(i) }));
+			}
+		}
+
+		if (totalPageCount > pageSize) {
+			if (lastPageNoOnPageList < totalPageCount) {
+				strBuff.append(MessageFormat.format(nextPageLabel, new Object[] { jsFunction, Integer.toString(firstPageNoOnPageList + pageSize) }));
+				strBuff.append(MessageFormat.format(lastPageLabel, new Object[] { jsFunction, Integer.toString(lastPageNo) }));
+			} else {
+				strBuff.append(MessageFormat.format(nextPageLabel, new Object[] { jsFunction, Integer.toString(lastPageNo) }));
+				strBuff.append(MessageFormat.format(lastPageLabel, new Object[] { jsFunction, Integer.toString(lastPageNo) }));
+			}
+		}
+		else{
+			strBuff.append(MessageFormat.format(nextPageLabel, new Object[] { jsFunction, Integer.toString(lastPageNo) }));
+			strBuff.append(MessageFormat.format(lastPageLabel, new Object[] { jsFunction, Integer.toString(lastPageNo) }));
+		}
+		return strBuff.toString();
 	}
 }
